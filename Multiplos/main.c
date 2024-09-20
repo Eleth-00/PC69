a482fc88075a0d97c05ad81594c0f93f976d59af
marty <martinhoduarte02@gmail.com>
2024-09-20 12:05:42 +0100

    multiplos 2, 3 e 5

--- Multiplos/main.c
+++ Multiplos/main.c
@@ -3,6 +3,15 @@
 #include <locale.h>
 void main()
 {
+    int v;
     setlocale(LC_ALL,"Portuguese");
+    printf("Insira um valor inteiro!");
+    scanf("%i",&v);
+    if(v%2==0)
+
+    if(v%3==0)
+
+    if(v%5==0)
+
 
 }
